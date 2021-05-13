355bad17fd0afdc51ea580422bb646a7ce058807
Lawrence O. Caulker <locaulker@gmail.com>
2021-05-13 17:53:21 -0400

    initial setup - installed gestalt and created navigation

---
+++ client/src/components/Signin.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Singin extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Signin</h2>
+      </div>
+    )
+  }
+}
+
+export default Singin
